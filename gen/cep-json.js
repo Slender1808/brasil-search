07d4e71283d4903b90b3d7c46a60d78d25fd9ee9
jadson <jadson.g-matos@outlook.com>
2022-10-16 19:02:47 +0000

    file

--- gen/cep-json.js
+++ gen/cep-json.js
@@ -1,14 +1,15 @@
+const jsonfile = require("jsonfile");
 const AVLTree = require("avl");
-var fs = require("fs");
+const fs = require("fs");
 
 //           99999999;
-const size = 50000000;
+//           50000000
+const size = 99999999;
 
 const tree = new AVLTree();
-
 var i = 0;
 
-for (let index = 0; index <= 99999999; index++) {
+for (let index = 0; index <= size; index++) {
   i++;
   if (i % 10000 == 0) {
     console.log("a", index);
@@ -18,28 +19,23 @@ for (let index = 0; index <= 99999999; index++) {
   tree.insert(index);
 }
 
-const root = 5000000; //tree.at(Math.round(size / 2)).key;
+const root = tree.at(Math.round(size / 2)).key;
+console.log("root", root);
 
-let data = [...Array(size)];
 i = 0;
-tree.forEach((node) => {
+tree.forEach(async (node) => {
   i++;
   if (i % 10000 == 0) {
     console.log("b", i);
   }
-  if (i > 50000000) {
-    data[i-50000001] = {
+
+  fs.appendFileSync(
+    "/tmp/cep.json",
+    JSON.stringify({
       node: node.key,
       parent: node.parent ? node.parent.key : root,
       left: node.left ? node.left.key : root,
       right: node.right ? node.right.key : root,
-    };
-  }
-});
-
-tree.destroy()
-
-fs.writeFile("./avl1.json", JSON.stringify(data), function (err) {
-  if (err) throw err;
-  console.log("complete");
+    }) + "\n"
+  );
 });
