e667312a58e58a6c731deae46279ba8e49fef2b0
Jadson G. Matos <50336793+jadsongmatos@users.noreply.github.com>
2022-10-26 19:57:34 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,10 @@
-import Script from "next/script";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
-  componentDidMount() {
-    this.loadFbLoginApi();
-  }
   render() {
     return (
       <Html>
         <Head>
           <meta name="google-site-verification" content="K6n_i0D944OJIJwD-M5iQ-jy3oAKFS5aTTL3uJOpy9I" />
-          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7540935582112706"
-     crossOrigin="anonymous"></Script>
         </Head>
         <body>
           <Main />
