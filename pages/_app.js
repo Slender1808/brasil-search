baa5d0240594d2a0bd0b0fb38f69423cf746c63e
jadson <jadson.g-matos@outlook.com>
2022-11-05 04:18:29 +0000

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import Script from "next/script";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Analytics from "../components/Analytics";
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3304167426675646"
-          crossorigin="anonymous"
-        ></Script>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3304167426675646"
+        crossorigin="anonymous"
+      ></Script>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
