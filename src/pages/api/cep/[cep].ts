11336572df37853a7d4c88cd531887944296acf7
jadsongmatos <jadson.g-matos@outlook.com>
2023-01-18 16:58:39 -0300

    cache

--- src/pages/api/cep/[cep].ts
+++ src/pages/api/cep/[cep].ts
@@ -5,21 +5,28 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { cep } = req.query;
+  const cep = Number(req.query.cep);
   console.log("cep", cep);
-  try {
-    fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`, {
-      cachePath: "/tmp/br/",
-      cache: "force-cache",
-    })
-      .then((res) => {
-        return res.json(); // download the body as JSON
-      })
-      .then((body) => {
-        res.status(200).json(body);
-      });
-  } catch (err: any) {
-    console.log("err", err);
-    res.status(500).send(err);
+  if (isNaN(cep)) {
+    res.status(500).send(cep);
+  } else {
+    try {
+      fetch(
+        `https://brasilapi.com.br/api/cep/v2/${String(cep).padStart(8, "0")}`,
+        {
+          cachePath: "/tmp/br/",
+          cache: "force-cache",
+        }
+      )
+        .then((res) => {
+          return res.json(); // download the body as JSON
+        })
+        .then((body) => {
+          res.status(200).json(body);
+        });
+    } catch (err: any) {
+      console.log("err", err);
+      res.status(500).send(err);
+    }
   }
 }
