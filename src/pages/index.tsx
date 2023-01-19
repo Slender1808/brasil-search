4b5cf8c7b4344b122b83cf2cd6968c8e7e0f257e
jadsongmatos <jadson.g-matos@outlook.com>
2023-01-19 02:36:29 -0300

    search dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <Header active="/" />
-      <main className="pt-5 mt-3 container">
+      <main className="py-5 my-5 container">
         <Gcse />
         <div className="gcse-search"></div>
       </main>
